4bcf6b700795161ebbe3834cd0534b48bb7acfbd
yoychen <yui12327@gmail.com>
2022-12-17 20:11:33 +0800

    feat: add jump feature
    refer: https://stackblitz.com/edit/pixijs-jump-example-qophdy?file=index.js

--- src/MoonBunny.ts
+++ src/MoonBunny.ts
@@ -2,7 +2,7 @@ import { Application, Assets, AnimatedSprite, Container } from "pixi.js";
 
 const createAnimatedSprite = async (
   path: string,
-  { x, y }: { x: number; y: number }
+  { x, y, animationSpeed }: { x: number; y: number; animationSpeed: number }
 ) => {
   const sprite = await Assets.load(path).then((sheet) => {
     return new AnimatedSprite(Object.values(sheet.textures) as any);
@@ -11,19 +11,22 @@ const createAnimatedSprite = async (
   sprite.anchor.y = 1;
   sprite.x = x;
   sprite.y = y;
-  sprite.animationSpeed = 0.3;
+  sprite.animationSpeed = animationSpeed;
 
   return sprite;
 };
 
 export class MoonBunny {
   _normalSprite!: AnimatedSprite;
-  _movingSprite!: AnimatedSprite;
+  _walkingSprite!: AnimatedSprite;
+  _jumpingSprite!: AnimatedSprite;
 
-  status: "STAND" | "MOVING" = "STAND";
+  status: "STAND" | "WALKING" | "JUMPING" = "STAND";
   direction: "LEFT" | "RIGHT" = "LEFT";
 
   workingSpeed = 3;
+  gravity = 1;
+  jumpPower = 20;
 
   async init(
     app: Application,
@@ -31,43 +34,73 @@ export class MoonBunny {
   ) {
     this._normalSprite = await createAnimatedSprite(
       "images/bunny_normal.json",
-      { x, y }
+      { x, y, animationSpeed: 0.3 }
     );
     this._normalSprite.play();
 
-    this._movingSprite = await createAnimatedSprite(
+    this._walkingSprite = await createAnimatedSprite(
       "images/bunny_moving.json",
-      { x, y }
+      { x, y, animationSpeed: 0.3 }
     );
-    this._movingSprite.play();
+    this._walkingSprite.play();
+
+    this._jumpingSprite = await createAnimatedSprite(
+      "images/bunny_moving.json",
+      { x, y, animationSpeed: 0.25 }
+    );
+    this._jumpingSprite.loop = false;
 
     this.registerListeners();
     app.ticker.add(this.gameLoop);
   }
 
+  cumulatedTime = 0;
+  jumpAt = 0;
+
   gameLoop = (delta: number) => {
     this.updateSpriteStatus();
 
-    if (this.status === "MOVING") {
+    if (this.status === "WALKING") {
       this.x +=
         delta * this.workingSpeed * (this.direction === "RIGHT" ? 1 : -1);
+    }
+
+    if (this.status === "JUMPING") {
+      const jumpHeight =
+        (-this.gravity / 2) * Math.pow(this.cumulatedTime, 2) +
+        this.jumpPower * this.cumulatedTime;
+      this.y = this.jumpAt + -1 * jumpHeight;
+
+      this.cumulatedTime += delta;
+      if (this.y > this.jumpAt) {
+        this.status = "STAND";
+        this.y = this.jumpAt;
+      }
     }
   };
 
   updateSpriteStatus() {
     if (this.direction === "LEFT") {
       this._normalSprite.scale.x = 1;
-      this._movingSprite.scale.x = 1;
+      this._walkingSprite.scale.x = 1;
+      this._jumpingSprite.scale.x = 1;
     } else {
       this._normalSprite.scale.x = -1;
-      this._movingSprite.scale.x = -1;
+      this._walkingSprite.scale.x = -1;
+      this._jumpingSprite.scale.x = -1;
     }
 
     if (this.status === "STAND") {
-      this._movingSprite.visible = false;
+      this._walkingSprite.visible = false;
+      this._jumpingSprite.visible = false;
       this._normalSprite.visible = true;
+    } else if (this.status === "WALKING") {
+      this._walkingSprite.visible = true;
+      this._jumpingSprite.visible = false;
+      this._normalSprite.visible = false;
     } else {
-      this._movingSprite.visible = true;
+      this._walkingSprite.visible = false;
+      this._jumpingSprite.visible = true;
       this._normalSprite.visible = false;
     }
   }
@@ -75,8 +108,15 @@ export class MoonBunny {
   registerListeners() {
     window.addEventListener("keydown", (event) => {
       if (event.code === "ArrowRight" || event.code === "ArrowLeft") {
-        this.status = "MOVING";
+        this.status = "WALKING";
         this.direction = event.code === "ArrowRight" ? "RIGHT" : "LEFT";
+      }
+
+      if (event.code === "ArrowUp") {
+        this.status = "JUMPING";
+        this._jumpingSprite.gotoAndPlay(0);
+        this.cumulatedTime = 0;
+        this.jumpAt = this._jumpingSprite.y;
       }
     });
 
@@ -92,7 +132,8 @@ export class MoonBunny {
 
   set x(value: number) {
     this._normalSprite.x = value;
-    this._movingSprite.x = value;
+    this._walkingSprite.x = value;
+    this._jumpingSprite.x = value;
   }
   get x() {
     return this._normalSprite.x;
@@ -100,14 +141,16 @@ export class MoonBunny {
 
   set y(value: number) {
     this._normalSprite.y = value;
-    this._movingSprite.y = value;
+    this._walkingSprite.y = value;
+    this._jumpingSprite.y = value;
   }
   get y() {
     return this._normalSprite.y;
   }
 
   addIntoStage(stage: Container) {
-    stage.addChild(this._movingSprite);
+    stage.addChild(this._walkingSprite);
+    stage.addChild(this._jumpingSprite);
     stage.addChild(this._normalSprite);
   }
 }
