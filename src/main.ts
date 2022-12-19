dbbce2ef4ef03c5585b693b58cb5b245777a71ee
yoychen <yui12327@gmail.com>
2022-12-19 23:37:34 +0800

    feat: add tile grounds

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Graphics } from "pixi.js";
+import { Application, TilingSprite } from "pixi.js";
 import { MoonBunny } from "./MoonBunny";
 import "./style.css";
 
@@ -21,20 +21,27 @@ await moonBunny.init(app, {
   y: app.renderer.height / 2,
 });
 
-let ground = new Graphics();
-ground.beginFill(0xffc107);
-ground.drawRect(0, 0, app.renderer.width, 50);
+const tileSize = {
+  width: 128,
+  height: 93,
+};
+const ground = TilingSprite.from("images/tiles/ground2.png", tileSize);
 ground.x = 0;
-ground.y = app.renderer.height / 2 + 100;
+ground.y = app.renderer.height / 2 + 150;
+ground.width = app.renderer.width;
 app.stage.addChild(ground);
 
-let ground2 = new Graphics();
-ground2.beginFill(0xffc107);
-ground2.drawRect(0, 0, app.renderer.width, 50);
-ground2.x = 600;
-ground2.y = app.renderer.height / 2 + 15;
-app.stage.addChild(ground2);
+const ground2Head = TilingSprite.from("images/tiles/ground1.png", tileSize);
+ground2Head.x = app.renderer.width / 2;
+ground2Head.y = app.renderer.height / 2 + 15;
+app.stage.addChild(ground2Head);
+const ground2Body = TilingSprite.from("images/tiles/ground2.png", tileSize);
+ground2Body.x = app.renderer.width / 2 + 128;
+ground2Body.y = app.renderer.height / 2 + 15;
+ground2Body.width = app.renderer.width / 2;
+app.stage.addChild(ground2Body);
 
 moonBunny.grounds.push(ground);
-moonBunny.grounds.push(ground2);
+moonBunny.grounds.push(ground2Head);
+moonBunny.grounds.push(ground2Body);
 moonBunny.addIntoStage(app.stage);
